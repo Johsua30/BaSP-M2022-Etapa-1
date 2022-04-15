4c778ab3b6f5c83f138f729b9ce45052c7cbc2a6
Johsua Nowak <johsua.nowak@gmail.com>
2022-04-15 10:06:10 -0300

    BaSP-2022: Created index.html file and finished first block of exercises

---
+++ semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,19 @@
+/* Crear dos variables numéricas y utilizar el operador suma 
+para guardar el valor de la suma de ambos números en una 3er variable. */
+var variableNum1 = 24, variableNum2 = 35, variableNum3;
+variableNum3 = variableNum1 + variableNum2;
+
+console.log(variableNum3)
+
+/* Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+var variableString1 = 'audio', variableString2 = 'visual', variableString3;
+variableString3 = variableString1 + variableString2;
+
+console.log(variableString3)
+
+/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+guardando el resultado de la suma en una 3er variable (utilizar length). */
+var variableString4 = 'casa', variableString5 = 'perro', variableString6;
+variableString6 = variableString4.length + variableString5.length
+
+console.log(variableString6)
