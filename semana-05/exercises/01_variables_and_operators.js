7671160d5f83edf38db1cbd57dff9d1f9557b52a
Johsua Nowak <johsua.nowak@gmail.com>
2022-04-16 09:30:40 -0300

    BaSP-2022: Finished Strings exercises and changed some variable names in the first group of exercises

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -1,4 +1,4 @@
-/* Crear dos variables numéricas y utilizar el operador suma 
+/* Crear dos variables numéricas y utilizar el operador suma
 para guardar el valor de la suma de ambos números en una 3er variable. */
 var variableNum1 = 24, variableNum2 = 35, variableNum3;
 variableNum3 = variableNum1 + variableNum2;
@@ -11,7 +11,7 @@ variableString3 = variableString1 + variableString2;
 
 console.log(variableString3)
 
-/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
 guardando el resultado de la suma en una 3er variable (utilizar length). */
 var variableString4 = 'casa', variableString5 = 'perro', variableString6;
 variableString6 = variableString4.length + variableString5.length
---
+++ semana-05/exercises/02_strings.js
@@ -0,0 +1,52 @@
+/* Crear una variable de tipo string con al menos 10 caracteres
+y convertir todo el texto en mayúscula (utilizar toUpperCase). */
+var stringsA1 = 'Una cadena de caracteres en minuscula';
+var stringsA2 = stringsA1.toUpperCase();
+
+console.log(stringsA2);
+
+/* Crear una variable de tipo string con al menos 10 caracteres y
+generar un nuevo string con los primeros 5 caracteres guardando el
+resultado en una nueva variable (utilizar substring). */
+var stringsB1 = 'Una cadena con muchos caracteres';
+var stringsB2 = stringsB1.substring(0, 5);
+
+console.log(stringsB2);
+
+/* Crear una variable de tipo string con al menos 10 caracteres y
+generar un nuevo string con los últimos 3 caracteres guardando el
+resultado en una nueva variable (utilizar substring). */
+var stringsC1 = 'Hay que quitar los ultimos caracteres';
+var stringsC2 = stringsC1.substring((stringsC1.length - 1), stringsC1.length - 4);
+
+console.log(stringsC2);
+
+/* Crear una variable de tipo string con al menos 10 caracteres y
+generar un nuevo string con la primera letra en mayúscula y las demás
+en minúscula. Guardar el resultado en una nueva variable (utilizar substring,
+toUpperCase, toLowerCase y el operador +). */
+var stringsD1 = 'se TRATA de UNA cadena.';
+var stringsD2 = (stringsD1.substring(0, 1)).toUpperCase() + (stringsD1.substring(1, stringsD1.length)).toLowerCase();
+
+console.log(stringsD2);
+
+/* Crear una variable de tipo string con al menos 10 caracteres y algún
+espacio en blanco. Encontrar la posición del primer espacio en blanco y
+guardarla en una variable (utilizar indexOf). */
+var stringsE1 = 'Encontrar la posición del primer espacio';
+var stringsE2 = stringsE1.indexOf(' ');
+
+console.log(stringsE2);
+
+/* Crear una variable de tipo string con al menos 2 palabras largas
+(10 caracteres y algún espacio entre medio). Utilizar los métodos de
+los ejercicios anteriores para generar un nuevo string que tenga la
+primera letra de ambas palabras en mayúscula y las demás letras en minúscula
+(utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +). */
+var stringsF1 = 'rAdIuM rOcKeT';
+var stringsF2 = stringsF1.substring(0, 1).toUpperCase() +
+stringsF1.substring(1, (stringsF1.indexOf(' ') + 1)).toLowerCase() +
+stringsF1.substring((stringsF1.indexOf(' ') + 1), (stringsF1.indexOf(' ') + 2)).toUpperCase() +
+stringsF1.substring((stringsF1.indexOf(' ') + 2), stringsF1.length).toLowerCase();
+
+console.log(stringsF2);
