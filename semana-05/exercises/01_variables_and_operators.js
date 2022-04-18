6a802f6c12be133a43940125a559c70d5f1f8144
Johsua Nowak <johsua.nowak@gmail.com>
2022-04-17 21:06:51 -0300

    BaSP-2022: Finished For group of exercises and added semicolons to the first group of exercises

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -3,17 +3,17 @@ para guardar el valor de la suma de ambos números en una 3er variable. */
 var variableNum1 = 24, variableNum2 = 35, variableNum3;
 variableNum3 = variableNum1 + variableNum2;
 
-console.log(variableNum3)
+console.log(variableNum3);
 
 /* Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
 var variableString1 = 'audio', variableString2 = 'visual', variableString3;
 variableString3 = variableString1 + variableString2;
 
-console.log(variableString3)
+console.log(variableString3);
 
 /* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
 guardando el resultado de la suma en una 3er variable (utilizar length). */
 var variableString4 = 'house', variableString5 = 'parrot', variableString6;
 variableString6 = variableString4.length + variableString5.length
 
-console.log(variableString6)
+console.log(variableString6);
