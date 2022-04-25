263267da11af491b8d8d204ad9e95f353d3ccb13
Johsua Nowak <johsua.nowak@gmail.com>
2022-04-25 19:19:53 -0300

    BaSP-2022: Finished with the javascript logic for the log in

--- semana-06/javascript/login.js
+++ semana-06/javascript/login.js
@@ -3,17 +3,10 @@ window.onload = function() {
     var emailWarning = document.getElementById('login-email-warning');
     var password = document.getElementById('login-password-field');
     var passwordWarning = document.getElementById('login-password-warning');
-    var loginButton = document.querySelector('.loginsubmit');
+    var loginButton = document.querySelector('.login-submit');
 
     var emailRegEx = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i;
-
-    // function validateEmail(emailToValidate) {
-    //     if ((emailToValidate.value).indexOf('@') == (-1) || (emailToValidate.value).indexOf('.') == (-1)) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
+    var passwordRegEx = /[^a-z0-9]/gi;
 
     function validateEmail(emailToValidate) {
         if (emailRegEx.test(emailToValidate.value)) {
@@ -23,14 +16,40 @@ window.onload = function() {
         }
     }
 
+    function validatePassword(passwordToValidate) {
+        if (passwordRegEx.test(passwordToValidate.value)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     function emailCheckBlur() {
         if (email.value == '') {
             emailWarning.textContent = 'Email field cannot be empty';
+            return ('Error: Email field is empty');
         } else {
             if (validateEmail(email)) {
                 emailWarning.textContent = 'Inputted text is not a valid email';
+                return ('Error: Inputted text is not a valid email');
             } else {
                 emailWarning.textContent = '';
+                return (email.value)
+            }
+        }
+    }
+
+    function passwordCheckBlur() {
+        if (password.value == '') {
+            passwordWarning.textContent = 'Password field cannot be empty';
+            return ('Error: Password field is empty');
+        } else {
+            if (validatePassword(password)) {
+                passwordWarning.textContent = 'Inputted password is not valid';
+                return ('Error: Inputted password is not valid');
+            } else {
+                passwordWarning.textContent = '';
+                return (password.value);
             }
         }
     }
@@ -43,14 +62,6 @@ window.onload = function() {
         }
     }
 
-    function passwordCheckBlur() {
-        if (password.value == '') {
-            passwordWarning.textContent = 'Password field cannot be empty';
-        } else {
-            passwordWarning.textContent = '';
-        }
-    }
-
     function passwordCheckFocus() {
         if (password.value === '') {
             passwordWarning.textContent = '';
@@ -59,9 +70,14 @@ window.onload = function() {
         }
     }
 
+    function submitMessage() {
+        alert('Email: ' + emailCheckBlur() + '\nPassword: ' + passwordCheckBlur());
+    }
+
     email.addEventListener("blur", emailCheckBlur);
     email.addEventListener("focus", emailCheckFocus);
     password.addEventListener("blur", passwordCheckBlur);
     password.addEventListener("focus", passwordCheckFocus);
+    loginButton.addEventListener("click", submitMessage);
 
 }
